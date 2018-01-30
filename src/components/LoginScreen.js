7eb3977cf4d571cbf9083e175c5fd90bb956e767
kim wonchang <wonchang.k@gmail.com>
2018-01-30 14:46:55 +0900

    로그인화면

---
+++ src/components/LoginScreen.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {Grid, Segment,Button, Header , Icon} from 'semantic-ui-react'
+import styled from  'styled-components';
+
+const FullHeightGrid = styled(Grid)`
+  height: 100vh;
+`
+
+export default class LoginScreen extends Component{
+  render(){
+    return(
+      <FullHeightGrid centered verticalAlign='middle'>
+          <Grid.Column style={{width: '280px'}}>
+            <Segment stacked textAlign='center' padded='very' color='red'>
+              <Header as='h1'>로그인</Header>
+              <Button fluid color='google plus'>
+                  <Icon name='google plus' />구글로 로그인
+              </Button>
+              <Button fluid color='twitter' style={{marginTop: '10px'}}>
+                   <Icon name='twitter' />트위터로 로그인
+              </Button>
+            </Segment>
+          </Grid.Column>
+      </FullHeightGrid>
+    )
+  }
+}
