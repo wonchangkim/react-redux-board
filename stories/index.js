ca1de77eba432ed6997d18218b21fdae2251c53b
kim wonchang <wonchang.k@gmail.com>
2018-01-30 12:18:50 +0900

    initial

---
+++ stories/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+
+
+storiesOf('Button', module).add('with text', () => (
+  <button onClick={action('clicked')}>Hello button</button>
+)).add('with some emoji', () => (
+  <button onClick={action('clicked')}>😀 😎 👍 💯</button>
+));
