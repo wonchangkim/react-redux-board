56ace7906ac60a00bce962699283822944bc3939
kim wonchang <wonchang.k@gmail.com>
2018-02-01 10:57:08 +0900

    루트경로 접속시 리다이렉트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import LoginScreenContainer from './containers/LoginScreenContainer';
 import ListPage from './containers/ListPage';
+import withAuth from './hocs/withAuth'
+
+const Home = withAuth(() =>
+  <Redirect to="/list" />
+)
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
+          <Route path="/" exact component={Home} />
           <Route path="/login" component={LoginScreenContainer} />
           <Route path="/list" component={ListPage} />
         </div>
