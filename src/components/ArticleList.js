543c6a81a0b16e10633ed1cbf7dd3a8a390e58bb
kim wonchang <wonchang.k@gmail.com>
2018-02-01 12:33:21 +0900

    ArticleList 완성

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,33 +1,24 @@
 import React, { Component } from 'react';
 import { List } from 'semantic-ui-react';
 
-const articles = [
-  {
-    id: 'acticle0',
-    title: '게시글 제목',
-    createdAt: 1517453827525,
-    nickName: '벼얼명',
-  }, {
-    id: 'acticle1',
-    title: '게시글 제목1',
-    createdAt: 1517453827525,
-    nickName: '벼얼명',
-  },
-];
 
 export default class ArticleList extends Component {
+  static defaultProps = {
+    articles: [],
+  }
   render() {
+    const { articles } = this.props;
     return (
       <List divided relaxed>
         {
           articles.map(({
-            id, title, createdAt, nickName,
+            id, title, createdAt, nickName, itemProps = {},
           }) => (
-            <List.Item key={id}>
+            <List.Item key={id} {...itemProps} >
               <List.Icon name="github" size="large" verticalAlign="middle" />
               <List.Content>
-                <List.Header as="a">{title} ({nickName})</List.Header>
-                <List.Description as="a">{createdAt}</List.Description>
+                <List.Header>{title} ({nickName})</List.Header>
+                <List.Description>{createdAt}</List.Description>
               </List.Content>
             </List.Item>
           ))
--- stories/ArticleList.js
+++ stories/ArticleList.js
@@ -3,5 +3,29 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import ActicleList from '../src/components/ArticleList';
 
+
+const articles = [
+  {
+    id: 'acticle0',
+    title: '게시글 제목',
+    createdAt: 1517453827525,
+    nickName: '벼얼명',
+  },
+  {
+    id: 'acticle1',
+    title: '게시글 제목1',
+    createdAt: 1517453827525,
+    nickName: '벼얼명',
+  },
+];
+// ({}) 는 객체가 된다.
+const articelsWithLink = articles.map(articel => ({
+  ...articel,
+  itemProps: {
+    as: 'a',
+    href: 'https://www.google.com',
+  },
+}));
 storiesOf('ActicleList', module)
-  .add('default', () => (<ActicleList />))
+  .add('default', () => (<ActicleList articles={articles} />))
+  .add('links', () => (<ActicleList articles={articelsWithLink} />));
